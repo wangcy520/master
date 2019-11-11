25e066091e97b628bbb9f66f0e00bbf24b68ba06
zi <852@163.com>
2019-11-11 19:10:51 +0800

    查询类型区分

--- src/main/java/com/wm/gameplat/controller/wage/ActivityController.java
+++ src/main/java/com/wm/gameplat/controller/wage/ActivityController.java
@@ -3,13 +3,15 @@ package com.wm.gameplat.controller.wage;
 
 import com.wm.gameplat.core.domain.ActivityInfo;
 import com.wm.gameplat.core.service.IActivityService;
-
 import com.wm.gameplat.utils.ResultUtil;
 import com.wm.gameplat.vo.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 
 /**
  * 标签活动信息表
@@ -28,11 +30,20 @@ public class ActivityController {
     @GetMapping("/list")
     @ResponseBody
     public Result selectActivityList(ActivityInfo activity) {
-
-
-        return ResultUtil.data(iActivityService.selectActivityList(activity));
-
+        List<ActivityInfo> collect =iActivityService.selectActivityList(activity);
+        List<ActivityInfo> notices =new ArrayList<>();
+        List<ActivityInfo>  activitys = new ArrayList<>();
+        collect.forEach(item ->{
+                if(item.getTypes().equals("0")){
+                    notices.add(item);
+                }else {
+                    activitys.add(item);
+                }
+        });
+        Map<String, List> mmap = new HashMap<>();
+        mmap.put("notices",notices);
+        mmap.put("activitys",activitys);
+        return ResultUtil.data(mmap);
     }
-
 
 }
